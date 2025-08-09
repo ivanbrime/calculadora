13131995bdab6879986620bd4ee778ae4cbc52c4
ivanbrime <ivanjogadorcaro@gmail.com>
2025-08-09 12:23:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let numero = parseFloat(prompt('digite um numero 1'));
+let numero2 = parseFloat(prompt('digite um numero 2'));
+
+let operaçao = prompt("digite '+' para adiçao e '-' para subtraçao");
+
+let=resultado;
+
+if (operaçao ==="-"){
+    resultado = numero - numero2
+    alert("resultado da subtraçao"+ resultado)
+}
+else {
+    alert("operaçao invalida");
+}
+
+
+if (operaçao ==="+") {
+    resultado = numero + numero2
+    alert("resultado da soma" - resultado);
+}
+
+else {
+    alert("operaçao invalida");
+}
